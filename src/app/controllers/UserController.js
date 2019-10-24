df53e4bfc2c486b95f761a49b151434c53ea6fee
lliuti <lucas_liuti@hotmail.com>
2019-10-24 00:38:29 -0300

    Update User method
    Creating Update method in UserController
    Also adding UserId in req.body after authentication

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,6 +17,27 @@ class UserController {
             provider 
         });
     };
+
+    async update(req, res) {
+        const { email, oldpassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (email != user.email) {
+            const userExists = await User.findOne({ where: { email: req.body.email } });
+            if (userExists) {
+                return res.status(400).json({ Error: "User already exists"});
+            } 
+        }
+
+        if (oldpassword && !(await user.checkPassword(oldpassword))) {
+            return res.status(400).json({ error: "Password does not match" });
+        };
+
+        const { id, name, provider } = await user.update(req.body);
+
+        return res.status(200).json({id, name, provider});
+    }
 }
 
 export default new UserController();
